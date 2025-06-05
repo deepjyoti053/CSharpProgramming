f4cef80cc99dd9c783bb6a7fbe93fbcc23d91d9f
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-05 06:45:19 +0530

    Exception

---
+++ Exception_Handling/Exception_Class.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Exception_Class
+    {
+        public void Exception_1()
+        {
+            Console.WriteLine("Enter the 1st Number : ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2nd Number : ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int z = x / y;
+                Console.WriteLine("Result : " + z);
+
+            }
+
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+            try 
+            
+            {
+                int k = x + y;
+                Console.WriteLine("Sum of No. is : "+ k);
+            
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+            }
+
+
+
+        }
+    
+    }
+}
---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception_Class Obj = new Exception_Class();
+            Obj.Exception_1();
+
+            Console.ReadKey();
+
+        }
+    }
+}
