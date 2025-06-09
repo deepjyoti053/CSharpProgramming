214960ba3b53292cf0da803bc4c039cf82477747
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-09 19:22:45 +0530

    Transpose of Matrics

---
+++ Transpose_Matrics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Transpose_Matrics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transpose Obj = new Transpose();
+            Obj.Matrics();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Transpose_Matrics/Transpose.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transpose_Matrics
+{
+    //Accept a 3 X 3 Matrix and display TRANSPOSE Matrix.
+
+    class Transpose
+    {
+        public void Matrics()
+        {
+            int[,] array = new int[3,3];
+            int[,] trans = new int[3,3];
+
+            Console.WriteLine("Enter the array element : ");
+
+            for (int i = 0; i<3; i++)
+
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine((i,j));
+                     array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Original Array");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++) 
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        
+            // Transpose
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    trans[j,i] = array[i,j];
+                }
+            }
+
+            Console.WriteLine("Transpose of the Matrics is : ");
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(trans[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }      
+}
