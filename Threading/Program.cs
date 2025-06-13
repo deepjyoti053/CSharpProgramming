849a206adb8266de15598dff2b5a1ba20836d102
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-13 17:06:25 +0530

    Thread

---
+++ Threading/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Threading_1 Obj = new Threading_1();
+            Obj.f();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Threading/Threading_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Threading_1
+    {
+        public void f()
+        {
+            Thread t1 = new Thread(new ThreadStart(f1));
+            Thread t2 = new Thread(new ThreadStart(f2));
+          
+            t1.Start();
+            t2.Start();
+
+        }
+        public void f1()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("f1 value is {0}", i);
+            }
+        }
+
+        public void f2()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("f2 value is {0}", i);
+            }
+        }
+
+    }
+}
