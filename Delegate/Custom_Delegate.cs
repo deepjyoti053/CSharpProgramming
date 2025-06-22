b596448669aaa54dde7a94df591394ff5b0ea7ad
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-22 19:13:11 +0530

    FoHTML Formating

---
+++ Delegate/Custom_Delegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+   internal class Custom_Delegate
+  {
+        int num = 10;
+
+        public void f()
+        {
+            Add(5);
+            Console.WriteLine(num);
+        }
+        public void Add(int x)
+        {
+            num = num + x;
+        }
+ 
+  }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Custom_Delegate Obj = new Custom_Delegate();
+            Obj.f();
+
+            Console.ReadKey();
+        }
+    }
+}
