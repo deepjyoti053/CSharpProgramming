d46978c67456e89a97518c8a17ba85929029b152
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-23 13:23:32 +0530

    Enum

---
+++ Enum1/Enum_Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1
+{
+    class Enum_Class
+    {
+        public enum Weekdays
+        { 
+          Sunday=1,
+          Monday=2,
+          Tuesday=3,
+          Wednesday=4,
+          Thrusday=5,
+          Friday=6,
+          Saturday=7
+        }
+    }
+}
---
+++ Enum1/Program.cs
@@ -0,0 +1,15 @@
+﻿using static Enum1.Enum_Class;
+
+namespace Enum1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine((int)Weekdays.Tuesday);
+            Console.ReadKey();
+
+        }
+    }
+}
