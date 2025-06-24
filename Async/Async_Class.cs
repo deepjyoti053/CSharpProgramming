3161766af7ed0459f9f024dfd1162abb720acba9
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-24 12:41:32 +0530

    Async

---
+++ Async/Async_Class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Async_Class
+    {
+        public async void f()
+        {
+              await Task.Run(new Action(f1));
+              await Task.Run(new Action(f2));
+              Task.Run(new Action(f3));
+              
+        }
+
+         public void f1()
+        {
+            for (int n1 = 1; n1 <= 3; n1++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("I am from f1 {0}, Value is " + n1);
+            }
+        }
+        public void f2()
+        {
+            for(int n2 = 1; n2 <= 3; n2++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("I am from f2 {0}, Value is " + n2);
+            }
+        }
+        public void f3()
+        {
+            for(int n3 = 1; n3 <= 3; n3++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine("I am from f3 {0}, Value is " + n3);
+            }
+        }
+    }
+}
---
+++ Async/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Async_Class Obj = new Async_Class();
+            Obj.f();
+
+            Console.ReadKey();
+        }
+    }
+}
