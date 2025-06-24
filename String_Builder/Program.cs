8f6feb6d672e2847561a44761a3f044bf8a180e4
Deep Jyoti <deepjyoti053@outlook.com>
2025-06-24 12:16:41 +0530

    string builder

---
+++ String_Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace String_Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String_Class MyObj = new String_Class();
+            //MyObj.f();
+            // MyObj.f1();
+             MyObj.f2();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ String_Builder/String_Class.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Builder
+{
+    class String_Class
+    {
+        public void f()
+        {
+            string name = string.Empty;
+            StringBuilder names = new StringBuilder();
+            Console.WriteLine("Enter the 3 names");
+            for (int i = 0; i < 3; i++)
+            {
+                name = Console.ReadLine();
+                names.Append(" ");
+                names.Append(name);
+            }
+            Console.WriteLine(names.ToString());
+        }
+
+        public void f1()
+        {
+            string s = "This is a book";
+            string w = "pen";
+
+            if (s.Contains(w))
+            {
+                Console.WriteLine(w +" "+ "is belongs to s.");
+            }
+
+            else 
+             {
+                Console.WriteLine(w + " "+"is not belongs to s.");
+             }
+        }
+
+        public void f2()
+        {
+            string s = "book ";
+            string w = "Book";
+
+            if (s.Trim().ToLower().Equals(w.Trim().ToLower()))
+            {
+                Console.WriteLine("s is equals to w");
+            }
+
+            else 
+            {
+                Console.WriteLine("s is not equals to w");
+            }
+
+        }
+
+        
+
+
+
+    }
+}
